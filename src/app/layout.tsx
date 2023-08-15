39c6ec37c82be5999894f40b7323dcad82879eef
Julien Sanchez <juliens@nchez.fr>
2023-08-15 22:56:05 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import StyledComponentsRegistry from '@/lib/registry';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
+import {Analytics} from '@vercel/analytics/react';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -21,6 +22,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="en">
       <body style={{margin: 0, backgroundColor: 'black'}} className={inter.className}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <Analytics />
       </body>
     </html>
   );
